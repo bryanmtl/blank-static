5eebcde76af9370d4f953ddee631ff8f38ecc15d
Nicholas Henry <nicholas.henry@gmail.com>
2010-02-16 16:46:38 -0500

    Made changes to templates to conform with how I setup Radiant

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -1,1 +1,24 @@
-// add your custom javascripts to this file
+(function($) {
+
+  // Google  
+  $(function() {
+
+    function addWatermark() {
+      if ($('#cse-search-box input.text').val() == '') {
+        $('#cse-search-box input.text').css("background-image", "url(http://www.google.com/coop/intl/en/images/google_custom_search_watermark.gif)");
+      };  
+    }
+
+    addWatermark();
+
+    $("#cse-search-box input.text").bind('focus', function(event) {
+      $("#cse-search-box input.text").css("background-image", "none" );
+    });
+
+    $("#cse-search-box input.text").bind('blur', function(event) {
+      addWatermark();
+    });
+
+  });
+    
+}) (jQuery);
